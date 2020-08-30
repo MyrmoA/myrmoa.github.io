87ae8876af4fdaceed363aba1f441fde64d022ba
MyrmoA <arie.myrmo@arthrex.com>
2020-08-30 18:52:35 -0400

    adds projects try deploy ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
 const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
   }
 ];
 
