a5cc2161bc1a45fbaea31d12a05e476e61144dc0
MyrmoA <arie.myrmo@arthrex.com>
2020-08-30 22:36:46 -0400

    adds Tests github action

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component'
+import { NavDrawerComponent } from './components/nav-bar/nav-drawer/nav-drawer.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { AboutComponent } from './about/about.component'
+import { ProjectsComponent } from './projects/projects.component'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        AboutComponent,
+        NavBarComponent,
+        FooterComponent,
+        NavDrawerComponent,
+        ProjectsComponent
       ],
     }).compileComponents();
   }));
 
+  // Sample Tests - to remove 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -24,12 +35,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('myrmoa');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('myrmoa app is running!');
   });
 });
