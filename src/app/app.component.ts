b5caddd0206e5837dd751b644d1fcb92139abc4b
MyrmoA <arie.myrmo@arthrex.com>
2020-08-30 23:52:04 -0400

    finishing up weekend #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   title = 'myrmoa';
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  isHomeRoute() {
+    return this.router.url === '/';
+  }
 
   @HostBinding("class.drawer-open")
   isDrawerOpen: boolean = false;
