f13d2495c15bd2787c1f25857bec6086049faed4
MyrmoA <arie.myrmo@arthrex.com>
2020-09-01 21:51:42 -0400

    convert to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'myrmoa';
@@ -16,15 +16,14 @@ export class AppComponent implements OnInit {
     return this.router.url === '/';
   }
 
-  @HostBinding("class.drawer-open")
+  @HostBinding('class.drawer-open')
   isDrawerOpen: boolean = false;
-  
+
   toggleDrawer(isDrawerOpen: boolean) {
     this.isDrawerOpen = isDrawerOpen;
   }
 
   getHomeText(): string {
-    return "Welcome! My name is Arie Myrmo, I am a software developer currently working for Arthrex in Southwest Florida";
+    return 'Welcome! My name is Arie Myrmo, I am a software developer currently working for Arthrex in Southwest Florida';
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,18 +10,9 @@ import { ProjectsComponent } from './projects/projects.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    HeaderComponent,
-    FooterComponent,
-    ProjectsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AboutComponent, HeaderComponent, FooterComponent, ProjectsComponent],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
