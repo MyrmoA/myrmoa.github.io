0a6919645ae7c36ade0f726bdac530f180239d0b
MyrmoA <arie.myrmo@arthrex.com>
2020-09-06 01:00:40 -0400

    more content and clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'myrmoa';
   constructor(private router: Router) {}
 
   ngOnInit() {}
@@ -15,14 +14,4 @@ export class AppComponent implements OnInit {
   isHomeRoute() {
     return this.router.url === '/';
   }
-
-  @HostBinding('class.drawer-open')
-  isDrawerOpen: boolean = false;
-
-  toggleDrawer(isDrawerOpen: boolean) {
-    this.isDrawerOpen = isDrawerOpen;
-  }
-
-  getHomeIntroText: string =
-    "Welcome! My name is Arie Myrmo.<br/><br/> I am a software developer currently working for Arthrex, a global medical device company located in Southwest Florida. You can learn more about me <a routerLink='/about' routerLinkActive='active'>here</a>. I write here, occasionally speak at conferences, and work on a lot of different projects. You can learn more about me here. If you appreciate my work, consider becoming a sponsor.";
 }
